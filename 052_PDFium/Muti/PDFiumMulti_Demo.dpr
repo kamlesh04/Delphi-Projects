8c7b4e3f142e4636d3843749ddf8d36b3175ee97
KG <kamleshgaonkar04@gmail.com>
2024-03-20 14:49:19 +0530

    Initial commit

---
+++ 052_PDFium/Muti/PDFiumMulti_Demo.dpr
@@ -0,0 +1,14 @@
+program PDFiumMulti_Demo;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  uMain in 'uMain.pas' {Form6};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm6, Form6);
+  Application.Run;
+end.
---
+++ 052_PDFium/Muti/uMain.pas
@@ -0,0 +1,27 @@
+unit uMain;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  Winsoft.FireMonkey.PDFium;
+
+type
+  TForm6 = class(TForm)
+    FPdf1: TFPdf;
+    FPdfView1: TFPdfView;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form6: TForm6;
+
+implementation
+
+{$R *.fmx}
+
+end.
